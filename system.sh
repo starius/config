f36592324a977d225e5d6644376e02e51a089d39
Boris Nagaev <bnagaev@gmail.com>
2012-04-01 01:24:33 +0400

    system.sh: add forgotten skype.list

--- system.sh
+++ system.sh
@@ -2,7 +2,7 @@ apt-get install etckeeper
 apt-get -y install vim tmux aptitude mercurial
 update-alternatives --set editor /usr/bin/vim.basic
 sed '/cdrom/s@^@#@' -i /etc/apt/sources.list
-cp google-*.list opera.list squeeze-backports.list /etc/apt/sources.list.d/
+cp *.list /etc/apt/sources.list.d/
 echo 'APT::Default-Release "stable";' > /etc/apt/apt.conf
 apt-get update
 wget https://bitbucket.org/starius/config/raw/tip/packages.dpkg -O - | \
