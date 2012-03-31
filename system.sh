0c441bdfc2ed82a41a10664fb85107fcc16c410a
Boris Nagaev <bnagaev@gmail.com>
2012-03-31 23:26:43 +0400

    system.sh: add forgotten apt-get update

--- system.sh
+++ system.sh
@@ -5,6 +5,7 @@ sed '/cdrom/s@^@#@' -i /etc/apt/sources.list
 cp google-*.list opera.list /etc/apt/sources.list.d/
 wget https://bitbucket.org/starius/config/raw/tip/packages.dpkg -O - | \
   dpkg --set-selections
+apt-get update
 apt-get -y dselect-upgrade
 if (! grep -q ' /tmp' /etc/fstab ); then
     echo 'tmpfs /tmp tmpfs defaults,size=10g 0 0' >> /etc/fstab
