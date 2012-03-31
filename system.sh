7994eab10ead3d0d37fafe6db81659f55acdfd1d
Boris Nagaev <bnagaev@gmail.com>
2012-03-31 22:50:07 +0400

    add system and environment installer scripts
    --HG--
    extra : rebase_source : e7c34f0c0898af6fe89026a2f1275acc78be6fe8

---
+++ system.sh
@@ -0,0 +1,16 @@
+apt-get install etckeeper
+apt-get -y install vim tmux aptitude mercurial
+update-alternatives --set editor /usr/bin/vim.basic
+sed '/cdrom/s@^@#@' -i /etc/apt/sources.list
+cp google-*.list opera.list /etc/apt/sources.list.d/
+wget https://bitbucket.org/starius/config/raw/tip/packages.dpkg -O - | dpkg --set-selections
+apt-get -y dselect-upgrade
+if (! grep -q ' /tmp' /etc/fstab ); then
+    echo 'tmpfs /tmp tmpfs defaults,size=10g 0 0' >> /etc/fstab
+fi
+
+wget http://atdot.ch/scr/files/0.8/skype-call-recorder-debian_0.8_i386.deb
+dpkg -i skype-call-recorder-debian_0.8_i386.deb
+apt-get -f -y install
+apt-file update
+
