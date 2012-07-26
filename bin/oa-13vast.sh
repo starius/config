03d03473ae3d77996927d876ff611060c4abd44a
Boris Nagaev <bnagaev@gmail.com>
2012-07-26 17:56:34 +0400

    add another server check

--- bin/oa-13vast.sh
+++ bin/oa-13vast.sh
@@ -3,3 +3,8 @@ if wget $u -O -|grep '13vast'; then
     play -n -c3 synth sin %-12 sin %-3 sin %-5 sin %-2 fade h 0.1 3 0.1
 fi
 
+u='http://dpmaster.deathmask.net/?game=openarena&server=188.165.243.176:27960'
+if wget $u -O -|grep '13vast'; then
+    play -n -c3 synth sin %-12 sin %-15 sin %-5 sin %-2 fade h 0.1 1 0.1
+fi
+
