e5402474f9962dce4bd7845a59f3c0871918d0cd
Boris Nagaev <bnagaev@gmail.com>
2012-10-13 03:43:51 +0400

    oa-13vast.sh: fix GeM's IP

--- bin/oa-13vast.sh
+++ bin/oa-13vast.sh
@@ -1,4 +1,4 @@
-u='http://dpmaster.deathmask.net/?game=openarena&server=46.105.111.21:27960'
+u='http://dpmaster.deathmask.net/?game=openarena&server=213.163.83.191:27960'
 if wget $u -O -|grep '13vast'; then
     play -n -c3 synth sin %-12 sin %-3 sin %-5 sin %-2 fade h 0.1 3 0.1
 fi
