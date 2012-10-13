d91b069dfdfeec9fa891b6e2e733f830cbbdec86
Boris Nagaev <bnagaev@gmail.com>
2012-10-13 15:44:30 +0400

    13vast: do nothing if OA is running

--- bin/oa-13vast.sh
+++ bin/oa-13vast.sh
@@ -1,5 +1,7 @@
 u='http://dpmaster.deathmask.net/?game=openarena&server=213.163.83.191:27960'
 if wget $u -O -|grep '13vast'; then
-    play -n -c3 synth sin %-12 sin %-3 sin %-5 sin %-2 fade h 0.1 3 0.1
+    if (! pgrep openarena ); then
+        play -n -c3 synth sin %-12 sin %-3 sin %-5 sin %-2 fade h 0.1 3 0.1
+    fi
 fi
 
