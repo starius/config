75df39467c7681d0d6bb84dfeaeab97d6701b43e
Boris Nagaev <bnagaev@gmail.com>
2012-11-18 01:00:09 +0400

    fix forward-* scripts (no checks)

--- bin/forward-from-this.sh
+++ bin/forward-from-this.sh
@@ -3,14 +3,5 @@
 # 2 host
 # 3 remote_port
 
-netstat -lnt | grep -q 127.0.0.1:$1
-if [[ $? -ne 0 ]]; then
-    echo Creating new tunnel connection
-    /usr/bin/ssh -f -N -L $1:localhost:$3 $2
-    if [[ $? -eq 0 ]]; then
-        echo Tunnel to hostb created successfully
-    else
-        echo An error occurred creating a tunnel to hostb RC was $?
-    fi
-fi
+/usr/bin/ssh -f -N -L $1:localhost:$3 $2
 
