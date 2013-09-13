c7f83294cce284254fb32f6d3d84e3be79c1906b
Boris Nagaev <bnagaev@gmail.com>
2013-09-13 14:05:17 +0400

    plowbackup: add --encrypt option

--- bin/plowbackup.py
+++ bin/plowbackup.py
@@ -5,6 +5,14 @@ import os
 import sys
 import argparse
 import tempfile
+from random import randint, choice
+import string
+
+# The characters to make up the random password
+chars = string.ascii_letters + string.digits
+
+def random_password():
+    return "".join(choice(chars) for x in range(randint(8, 12)))
 
 def list_files(base_dir):
     result = []
@@ -19,7 +27,6 @@ def list_files(base_dir):
     return result
 
 def plowup(args, file):
-    file = os.path.join(args.dir, file)
     output = tempfile.NamedTemporaryFile(delete=False)
     os.system("bash -c 'plowup Sendspace %(file)s | tail -n 1 &> %(output)s'" %
             {'file': file, 'output': output.name})
@@ -34,9 +41,31 @@ def backup_file(args, file):
     if dir:
         o.write('mkdir -p %s\n' % dir)
         dir_opt = '-o ' + dir
-    url = plowup(args, file)
-    o.write('plowdown %(dir_opt)s %(url)s\n' %
-            {'dir_opt': dir_opt, 'url': url})
+    upload_file = os.path.join(args.dir, file)
+    # encrypt
+    encrypted = ''
+    key = ''
+    if args.encrypt:
+        encrypted = tempfile.NamedTemporaryFile(delete=False)
+        key = random_password()
+        os.system("cat %(upload_file)s | ccrypt -e -K %(key)s > %(encrypted)s" %
+                {'upload_file': upload_file, 'key': key,
+                 'encrypted': encrypted.name})
+        upload_file = encrypted.name
+    # upload
+    url = plowup(args, upload_file)
+    # delete tmp files
+    if args.encrypt:
+        os.unlink(encrypted.name)
+    if args.encrypt:
+        o.write('plowdown -o $tmpdir %s\n' % url)
+        o.write('f=$(find $tmpdir -type f)\n')
+        o.write('cat $f|ccrypt -d -K %(key)s > %(file)s\n' %
+                {'key': key, 'file': file})
+        o.write('rm $f\n')
+    else:
+        o.write('plowdown %(dir_opt)s %(url)s\n' %
+                {'dir_opt': dir_opt, 'url': url})
 
 r = argparse.FileType('r')
 w = argparse.FileType('w')
@@ -47,11 +76,16 @@ p.add_argument('-v','--version',action='version',version='%(prog)s 1.0')
 p.add_argument('--dir',help='Directory',metavar='DIR', default='.')
 p.add_argument('--out',help='Output file for script',
         metavar='FILE',type=w,default='-')
+p.add_argument('--encrypt',help='Encrypt files with ccrypt',
+        metavar='DIR',type=bool,default=True)
 
 args = p.parse_args()
 base_dir = args.dir
 
 files = list_files(base_dir)
 for file in files:
+    o = args.out
+    o.write('tmpdir=$(mktemp -d)\n')
     backup_file(args, file)
+    o.write('rmdir $tmpdir\n')
 
