b2ea23cfff1b4b36de0a423d8094a7a9f44278a8
Boris Nagaev <bnagaev@gmail.com>
2014-11-30 02:56:32 +0300

    scripts allglobals.sh, globals.sh
    do not depend on findglobals.lua

---
+++ bin/allglobals.sh
@@ -0,0 +1,1 @@
+luac -l -p $1 | egrep ETGLOBAL
--- bin/globals.sh
+++ bin/globals.sh
@@ -1,4 +1,4 @@
-luac -l -p $1 | findglobals.lua $1 | \
-    grep ETGLOBAL | awk '{print $6}' | \
+allglobals.sh $1 | \
+    awk '{print $7}' | \
     egrep -v \
     "string|xpcall|package|tostring|print|os|unpack|require|getfenv|setmetatable|next|assert|tonumber|io|rawequal|collectgarbage|getmetatable|module|rawset|math|debug|pcall|table|newproxy|type|coroutine|_G|select|gcinfo|pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|load|error|loadfile"
