97b5b043f0129e93dcf62fdf7514f2e782db17f9
Boris Nagaev <bnagaev@gmail.com>
2015-04-12 16:31:05 +0300

    fix false negatives in globals.sh
    globals.sh discards known Lua global variables, such as
    "print". It did discard also global variables with names
    including names of known Lua variables (example: "orig_print").

--- bin/globals.sh
+++ bin/globals.sh
@@ -1,4 +1,4 @@
 allglobals.sh $@ | \
     awk '{print $7}' | \
-    egrep -v \
+    egrep -wv \
     "string|xpcall|package|tostring|print|os|unpack|require|getfenv|setmetatable|next|assert|tonumber|io|rawequal|collectgarbage|getmetatable|module|rawset|math|debug|pcall|table|newproxy|type|coroutine|_G|select|gcinfo|pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|load|error|loadfile"
