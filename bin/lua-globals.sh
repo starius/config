45ad57286ab16302772da89d0bf4f7621fb20276
Boris Nagaev <bnagaev@gmail.com>
2015-06-14 17:21:36 +0300

    lua-globals.sh: print filename

--- bin/lua-globals.sh
+++ bin/lua-globals.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-(for f in $@; do luac -l -p $f; done) | egrep ETGLOBAL | \
-    awk '{print $2 " " $7}' | \
+for f in $@; do
+    luac -l -p $f | egrep ETGLOBAL | \
+    awk '{print "'$f' " $2 " " $7}' | \
     ( ! egrep -wv \
     "string|xpcall|package|tostring|print|os|unpack|require|getfenv|setmetatable|next|assert|tonumber|io|rawequal|collectgarbage|getmetatable|module|rawset|math|debug|pcall|table|newproxy|type|coroutine|_G|select|gcinfo|pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|load|error|loadfile" )
+done
