2b96a0431ecccc0b0a669b82d3bd73d1484dcd63
Boris Nagaev <bnagaev@gmail.com>
2015-11-17 02:03:43 +0300

    lua-globals.sh: fix false negative
    A path contains e.g. "io".

--- .bin/lua-globals.sh
+++ .bin/lua-globals.sh
@@ -3,6 +3,6 @@
 for f in $@; do
     luac -l -p $f | egrep ETGLOBAL | \
     awk '{print "'$f' " $2 " " $7}' | \
-    ( ! egrep -wv \
-    "string|xpcall|package|tostring|print|os|unpack|require|getfenv|setmetatable|next|assert|tonumber|io|rawequal|collectgarbage|getmetatable|module|rawset|math|debug|pcall|table|newproxy|type|coroutine|_G|select|gcinfo|pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|load|error|loadfile" )
+    ( ! egrep -v \
+    '] (string|xpcall|package|tostring|print|os|unpack|require|getfenv|setmetatable|next|assert|tonumber|io|rawequal|collectgarbage|getmetatable|module|rawset|math|debug|pcall|table|newproxy|type|coroutine|_G|select|gcinfo|pairs|rawget|loadstring|ipairs|_VERSION|dofile|setfenv|load|error|loadfile)$' )
 done
