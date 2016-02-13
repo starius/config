b4071e70bd975da079533f8b0a83f9194fd9b2b7
Boris Nagaev <bnagaev@gmail.com>
2016-02-14 01:06:25 +0300

    add script pasta.sh uploading to https://pasta.cf

---
+++ .bin/pasta.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -ue
+
+# Usage:
+# pasta.sh file
+# pasta.sh file --password
+# pasta.sh file --self-burning
+# cat file | pasta.sh
+# cat file | pasta.sh --password
+# cat file | pasta.sh --self-burning
+
+filename=""
+content="-"
+pasta_type="standard"
+mask='^view:'
+server="https://pasta.cf"
+
+for o in "$@"; do
+    case "$o" in
+
+        --password)
+            pasta_type="editable"
+            mask="^(password|view):"
+        ;;
+
+        --self-burning)
+            pasta_type="self_burning"
+            mask="^(raw|view):"
+        ;;
+
+        --local)
+            server="http://127.0.0.1:25516"
+        ;;
+
+        *)
+            filename=$(basename "$o")
+            content="$o"
+        ;;
+    esac
+done
+
+response=$(curl \
+    --silent --show-error \
+    $server/api/create \
+    -F "content=<$content" \
+    -F "filename=$filename" \
+    -F "pasta_type=$pasta_type")
+echo "$response" \
+| egrep "$mask" \
+| sort --reverse
