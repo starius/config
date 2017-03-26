1d315d8e2438128d370a37baff314d073d88f621
Boris Nagaev <bnagaev@gmail.com>
2017-03-26 06:07:18 +0200

    pia-connect: add zone sets to help message

--- .bin/pia-connect/main.go
+++ .bin/pia-connect/main.go
@@ -128,6 +128,9 @@ func getZones(cacheDir string) ([]string, error) {
 		return nil, fmt.Errorf("file %s exists but can't be read: %s", zonesFile, err)
 	}
 	printZones()
+	fmt.Println("North America: us-california,us-east,us-midwest,ca,ca-toronto")
+	fmt.Println("Europe: italy,nl,ro,germany,fi,no,denmark,swiss,sweden")
+	fmt.Println("Asia: aus,aus-melbourne,nz,kr,hk,sg,japan,in")
 	fmt.Printf("Please choose zones (for instance nl,brazil): ")
 	reader := bufio.NewReader(os.Stdin)
 	zonesStr, err := reader.ReadString('\n')
