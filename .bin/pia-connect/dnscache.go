e57efe3bee3af73ee61ff06336be4625025d176b
Boris Nagaev <bnagaev@gmail.com>
2017-04-15 04:53:16 +0200

    pia-connect: provide opt-out for DNS caching

--- .bin/pia-connect/dnscache.go
+++ .bin/pia-connect/dnscache.go
@@ -15,7 +15,7 @@ var (
 	timeout   = flag.Duration("dns-timeout", 60*time.Second, "Timeout for proxified DNS requests.")
 	upstream  = flag.String("dns-upstream", "8.8.8.8:53", "Upstream DNS server.")
 	listenOn  = flag.String("dns-listen", "0.0.0.0:53", "Where to run local DNS server.")
-	cacheSize = flag.Int("dns-cache", 1000, "Number of records in DNS cache.")
+	cacheSize = flag.Int("dns-cache", 1000, "Number of records in DNS cache (0 to disable).")
 )
 
 type DNSProxy struct {
@@ -31,11 +31,13 @@ func (d *DNSProxy) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	key := req.Question[0] // Note that cache can deanonymize multiple Qubes VMs to each other.
 	if len(req.Question) == 1 {
 		// req.Question with multiple values is rare and is hard to use as a key.
-		if cached, has := d.cache.Get(key); has {
-			clone := *cached.(*dns.Msg)
-			clone.Id = req.Id
-			w.WriteMsg(&clone)
-			return
+		if d.cache != nil {
+			if cached, has := d.cache.Get(key); has {
+				clone := *cached.(*dns.Msg)
+				clone.Id = req.Id
+				w.WriteMsg(&clone)
+				return
+			}
 		}
 	}
 	response, _, err := d.client.Exchange(req, *upstream)
@@ -46,18 +48,22 @@ func (d *DNSProxy) ServeDNS(w dns.ResponseWriter, req *dns.Msg) {
 	if len(req.Question) == 1 && len(response.Answer) > 0 {
 		ttl := response.Answer[0].Header().Ttl
 		expiresAt := time.Now().Add(time.Duration(ttl) * time.Second)
-		d.cache.Add(key, response, expiresAt)
+		if d.cache != nil {
+			d.cache.Add(key, response, expiresAt)
+		}
 	}
 	w.WriteMsg(response)
 }
 
 func RunDNS() error {
 	proxy := &DNSProxy{
-		cache: inmem.NewLocked(*cacheSize),
 		client: dns.Client{
 			ReadTimeout:  *timeout,
 			WriteTimeout: *timeout,
 		},
+	}
+	if *cacheSize > 0 {
+		proxy.cache = inmem.NewLocked(*cacheSize)
 	}
 	return dns.ListenAndServe(*listenOn, "udp", proxy)
 }
