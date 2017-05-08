22e5d49fcf1f461d5593c9f36910e476118b2564
Boris Nagaev <bnagaev@gmail.com>
2017-05-08 22:04:56 +0200

    add script encrypted-swap.sh

---
+++ .bin/encrypted-swap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo dd if=/dev/zero of=/encrypted-swap count=0 bs=1G seek=1
+sudo losetup /dev/loop0 /encrypted-swap
+
+KEY="$(head --bytes 100 /dev/urandom)"
+echo "$KEY" | sudo cryptsetup --batch-mode luksFormat -c aes -s 256 /dev/loop0 -d -
+echo "$KEY" | sudo cryptsetup --batch-mode luksOpen /dev/loop0 fun -d -
+
+sudo swapoff -a
+sudo mkswap /dev/mapper/fun
+sudo swapon /dev/mapper/fun
