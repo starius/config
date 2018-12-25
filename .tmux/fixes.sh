fa96bf97dd99f50b28f7652c544347a34efedb1f
Boris Nagaev <bnagaev@gmail.com>
2018-12-25 04:29:04 +0100

    tmux: fix PWD preservation if there is no bc

--- .tmux/fixes.sh
+++ .tmux/fixes.sh
@@ -3,6 +3,8 @@
 tmux_home=~/.tmux
 tmux_version="$(tmux -V | cut -c 6-)"
 
-if [[ $(echo "$tmux_version >= 1.9" | bc) -eq 1 ]] ; then
+# If the version is >= 1.9:
+
+if echo -e "${tmux_version}\n1.9" | sort -V | head -1 | egrep '^1.9$'; then
     tmux source-file "$tmux_home/cd_pwd.conf"
 fi
