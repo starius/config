3f27ccaeaf65512d4d344d033acf9d73103520fa
Boris Nagaev <bnagaev@gmail.com>
2020-05-02 02:56:16 +0200

    template: install inkscape

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,4 +1,4 @@
-sudo aptitude install gcc autossh mplayer git tmux vim torchat mercurial python-qt4 python-pip python-qrcode qbittorrent pidgin pidgin-otr gimp mat ristretto geany sshfs curl ruby chromium unzip openvpn openssl libssl-dev python3-setuptools python3-pyqt5 python3-pip python-msgpack jq daemonize graphviz yarnpkg ffmpeg rsync dnsutils mosh p7zip-full
+sudo aptitude install gcc autossh mplayer git tmux vim torchat mercurial python-qt4 python-pip python-qrcode qbittorrent pidgin pidgin-otr gimp inkscape mat ristretto geany sshfs curl ruby chromium unzip openvpn openssl libssl-dev python3-setuptools python3-pyqt5 python3-pip python-msgpack jq daemonize graphviz yarnpkg ffmpeg rsync dnsutils mosh p7zip-full
 sudo aptitude remove icedove thunderbird nano avahi-daemon unattended-upgrades
 sudo systemctl disable systemd-resolved tor.service
 sudo sed 's@RUN_DAEMON="yes"@RUN_DAEMON="no"@' -i /etc/default/tor
