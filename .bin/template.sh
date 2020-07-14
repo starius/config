3309e084873a21e8d4a23fc58eacba9156594871
Boris Nagaev <bnagaev@gmail.com>
2020-07-14 16:44:50 +0200

    template: split GUI and non-GUI

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,4 +1,5 @@
-sudo aptitude install gcc autossh mplayer git tmux vim torchat mercurial python-qt4 python-pip python-qrcode qbittorrent pidgin pidgin-otr gimp inkscape mat ristretto geany sshfs curl ruby chromium unzip openvpn openssl libssl-dev python3-setuptools python3-pyqt5 python3-pip python-msgpack jq daemonize graphviz yarnpkg ffmpeg rsync dnsutils whois mosh p7zip-full
+sudo aptitude install gcc autossh git tmux vim mercurial python-pip mat sshfs curl ruby unzip openvpn openssl libssl-dev python3-setuptools python3-pip python-msgpack jq daemonize graphviz yarnpkg ffmpeg rsync dnsutils whois mosh p7zip-full
+sudo aptitude install mplayer torchat python-qt4 python-qrcode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium python3-pyqt5
 sudo aptitude remove icedove thunderbird nano avahi-daemon unattended-upgrades
 sudo systemctl disable systemd-resolved tor.service
 sudo sed 's@RUN_DAEMON="yes"@RUN_DAEMON="no"@' -i /etc/default/tor
