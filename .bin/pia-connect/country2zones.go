d40a33901bf9dec75bca7701494121acb57ac0d7
Boris Nagaev <bnagaev@gmail.com>
2020-09-10 18:42:18 +0200

    pia-connect: update zones and servers

--- .bin/pia-connect/country2zones.go
+++ .bin/pia-connect/country2zones.go
@@ -5,15 +5,18 @@ var COUNTRY2ZONES = map[string][]string{
 	"United States": []string{
 		"us-california",
 		"us-east",
-		"us-midwest",
 		"us-chicago",
-		"us-texas",
+		"us-dal",
 		"us-florida",
-		"us-seattle",
+		"us-sea",
 		"us-west",
 		"us-siliconvalley",
-		"us-newyorkcity",
+		"us-nyc",
+		"us-washingtondc",
 		"us-atlanta",
+		"us-lasvegas",
+		"us-houston",
+		"us-denver",
 	},
 	"United Kingdom": []string{
 		"uk-london",
@@ -21,19 +24,31 @@ var COUNTRY2ZONES = map[string][]string{
 		"uk-manchester",
 	},
 	"Canada": []string{
+		"ca-ontario",
 		"ca-toronto",
-		"ca",
+		"ca-montreal",
 		"ca-vancouver",
 	},
 	"Australia": []string{
-		"aus",
-		"aus-melbourne",
+		"au-sydney",
+		"au-melbourne",
+		"au-perth",
+	},
+	"Germany": []string{
+		"de-berlin",
+		"de-frankfurt",
 	},
 	"New Zealand": []string{
 		"nz",
 	},
+	"Albania": []string{
+		"albania",
+	},
 	"Netherlands": []string{
 		"nl",
+	},
+	"Iceland": []string{
+		"is",
 	},
 	"Sweden": []string{
 		"sweden",
@@ -53,14 +68,17 @@ var COUNTRY2ZONES = map[string][]string{
 	"France": []string{
 		"france",
 	},
-	"Germany": []string{
-		"germany",
-	},
 	"Belgium": []string{
 		"belgium",
 	},
 	"Austria": []string{
 		"austria",
+	},
+	"Czech Republic": []string{
+		"czech",
+	},
+	"Luxembourg": []string{
+		"lu",
 	},
 	"Ireland": []string{
 		"ireland",
@@ -68,34 +86,79 @@ var COUNTRY2ZONES = map[string][]string{
 	"Italy": []string{
 		"italy",
 	},
+	"Portugal": []string{
+		"pt",
+	},
 	"Spain": []string{
 		"spain",
+	},
+	"Moldova": []string{
+		"md",
 	},
 	"Romania": []string{
 		"ro",
 	},
-	"Turkey": []string{
-		"turkey",
+	"Bulgaria": []string{
+		"bg",
 	},
-	"Hong Kong": []string{
-		"hk",
+	"Serbia": []string{
+		"rs",
+	},
+	"Latvia": []string{
+		"lv",
+	},
+	"Estonia": []string{
+		"ee",
+	},
+	"Lithuania": []string{
+		"lt",
+	},
+	"Slovakia": []string{
+		"sk",
+	},
+	"Hungary": []string{
+		"hungary",
+	},
+	"Poland": []string{
+		"poland",
+	},
+	"Greece": []string{
+		"gr",
+	},
+	"Bosnia and Herzegovina": []string{
+		"ba",
+	},
+	"Ukraine": []string{
+		"ua",
+	},
+	"North Macedonia": []string{
+		"mk",
+	},
+	"United Arab Emirates": []string{
+		"ae",
 	},
 	"Singapore": []string{
 		"sg",
 	},
 	"Japan": []string{
-		"japan",
+		"jp",
 	},
 	"Israel": []string{
 		"israel",
 	},
 	"Mexico": []string{
-		"mexico",
+		"mx",
 	},
 	"Brazil": []string{
-		"brazil",
+		"br",
+	},
+	"Argentina": []string{
+		"ar",
 	},
 	"India": []string{
 		"in",
+	},
+	"South Africa": []string{
+		"za",
 	},
 }
