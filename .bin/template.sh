1f7fb930a5af4110031f4f68dbf7f3256ad4e9e5
Boris Nagaev <bnagaev@gmail.com>
2020-12-04 19:41:55 +0100

    template.sh: split installed packages to groups

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,6 +1,7 @@
-sudo aptitude install gcc autossh git tmux vim mercurial python-pip mat sshfs curl ruby unzip openvpn openssl libssl-dev python3-setuptools python3-pip python-msgpack jq daemonize graphviz yarnpkg ffmpeg rsync dnsutils whois mosh p7zip-full
-sudo aptitude install mplayer torchat python-qt4 python-qrcode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium python3-pyqt5
-sudo aptitude remove icedove thunderbird nano avahi-daemon unattended-upgrades
+sudo apt-get install git tmux vim mercurial curl unzip openssl libssl-dev jq daemonize graphviz dnsutils whois p7zip-full build-essential
+sudo apt-get install autossh python-pip mat sshfs ruby openvpn python3-setuptools python3-pip python-msgpack yarnpkg ffmpeg rsync mosh
+sudo apt-get install mplayer torchat python-qt4 python-qrcode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium python3-pyqt5
+sudo apt-get remove icedove thunderbird nano avahi-daemon unattended-upgrades
 sudo systemctl disable systemd-resolved tor.service
 sudo sed 's@RUN_DAEMON="yes"@RUN_DAEMON="no"@' -i /etc/default/tor
 
