53b51f2de0380dd9038bbd5f98f3925d842115c9
Boris Nagaev <bnagaev@gmail.com>
2022-02-07 11:30:38 +0100

    template: do not remove icedove and thunderbird
    icedove does not exist in Debian 11 and thunderbird was a dependency of
    some core qubes package and is not harmful.

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,7 +1,7 @@
 sudo apt-get install git tmux vim mercurial curl unzip openssl libssl-dev jq daemonize graphviz dnsutils whois p7zip-full build-essential
 sudo apt-get install autossh mat sshfs ruby openvpn python3-setuptools yarnpkg ffmpeg rsync mosh
 sudo apt-get install mplayer qrencode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium python3-pyqt5
-sudo apt-get remove icedove thunderbird nano avahi-daemon unattended-upgrades
+sudo apt-get remove nano avahi-daemon unattended-upgrades
 sudo systemctl disable systemd-resolved tor.service
 sudo sed 's@RUN_DAEMON="yes"@RUN_DAEMON="no"@' -i /etc/default/tor
 
