5e5b23c34ed4da690ef63e2f2ac1eddfa72b5fd3
Boris Nagaev <bnagaev@gmail.com>
2022-02-07 11:31:55 +0100

    template: remove disabling of tor
    It is not installed by the script in its current state.

--- .bin/template.sh
+++ .bin/template.sh
@@ -2,7 +2,6 @@ sudo apt-get install git tmux vim mercurial curl unzip openssl libssl-dev jq dae
 sudo apt-get install autossh mat sshfs ruby openvpn python3-setuptools yarnpkg ffmpeg rsync mosh
 sudo apt-get install mplayer qrencode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium python3-pyqt5
 sudo apt-get remove nano avahi-daemon unattended-upgrades
-sudo systemctl disable systemd-resolved tor.service
-sudo sed 's@RUN_DAEMON="yes"@RUN_DAEMON="no"@' -i /etc/default/tor
+sudo systemctl disable systemd-resolved
 
 # https://www.qubes-os.org/doc/vm-sudo/#replacing-password-less-root-access-with-dom0-user-prompt
