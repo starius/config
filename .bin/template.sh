0d90e3714b7801cdd5f3d691f374be1a9df63da5
Boris Nagaev <bnagaev@gmail.com>
2022-04-10 22:41:10 +0200

    template: install ncal and libreoffice-impress

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,6 +1,6 @@
-sudo apt-get install git git-lfs tmux vim mercurial curl unzip openssl libssl-dev jq daemonize graphviz dnsutils whois p7zip-full build-essential
+sudo apt-get install git git-lfs tmux vim mercurial curl unzip openssl libssl-dev jq daemonize graphviz dnsutils whois p7zip-full build-essential ncal
 sudo apt-get install autossh mat sshfs openvpn python3-setuptools ffmpeg rsync mosh resolvconf
-sudo apt-get install mplayer qrencode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium abiword gnumeric
+sudo apt-get install mplayer qrencode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium abiword gnumeric libreoffice-impress
 sudo apt-get install golang-go ruby yarnpkg python3-pyqt5 python3-pip protobuf-compiler protoc-gen-go clang-format
 sudo apt-get remove nano avahi-daemon unattended-upgrades
 sudo systemctl disable systemd-resolved
