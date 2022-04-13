36d6c3e7ee107f43e64aa1a9b9db43e019b54771
Boris Nagaev <bnagaev@gmail.com>
2022-04-13 20:58:15 +0200

    template: install default-jre

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git git-lfs tmux vim mercurial curl unzip openssl libssl-dev jq daemonize graphviz dnsutils whois p7zip-full build-essential ncal
+sudo apt-get install git git-lfs tmux vim mercurial curl unzip openssl libssl-dev jq daemonize graphviz dnsutils whois p7zip-full build-essential ncal default-jre
 sudo apt-get install autossh mat sshfs openvpn python3-setuptools ffmpeg rsync mosh resolvconf
 sudo apt-get install mplayer qrencode qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium abiword gnumeric libreoffice-impress
 sudo apt-get install golang-go ruby yarnpkg python3-pyqt5 python3-pip protobuf-compiler protoc-gen-go clang-format
