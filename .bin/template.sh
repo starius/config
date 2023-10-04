198f4708330b3d90507d0b9886939f52baa6d45d
Boris Nagaev <bnagaev@gmail.com>
2023-10-04 16:38:15 +0200

    template: add xxd

--- .bin/template.sh
+++ .bin/template.sh
@@ -1,4 +1,4 @@
-sudo apt-get install git git-lfs tmux vim mercurial curl wget unzip openssl libssl-dev jq dos2unix daemonize graphviz dnsutils whois p7zip-full build-essential cmake ncal default-jre
+sudo apt-get install git git-lfs tmux vim mercurial curl wget unzip openssl libssl-dev jq xxd dos2unix daemonize graphviz dnsutils whois p7zip-full build-essential cmake ncal default-jre
 sudo apt-get install autossh mat sshfs openvpn python3-setuptools ffmpeg rsync mosh resolvconf gocryptfs
 sudo apt-get install mplayer qrencode qtqr qbittorrent pidgin pidgin-otr gimp inkscape ristretto geany chromium abiword gnumeric libreoffice-impress heif-gdk-pixbuf
 sudo apt-get install golang-go ruby yarnpkg python3-pyqt5 python3-pip protobuf-compiler protoc-gen-go clang-format
