de5128d8cca0304c883e795cb667091c5180421e
Boris Nagaev <bnagaev@gmail.com>
2026-03-03 00:44:59 -0500

    nix: update install script 2.29.1 -> 2.34.0
    curl -L https://nixos.org/nix/install > nix-install.sh

--- nix/nix-install.sh
+++ nix/nix-install.sh
@@ -26,43 +26,43 @@ require_util() {
 
 case "$(uname -s).$(uname -m)" in
     Linux.x86_64)
-        hash=d8a7e7349e8186ee669517fec033123203ab1ab5eb061b4edafb592da877524a
-        path=kbl336lsimpd08qwfbfnhy8smdarvbxy/nix-2.29.1-x86_64-linux.tar.xz
+        hash=5676b0887f1274e62edd175b6611af49aa8170c69c16877aa9bc6cebceb19855
+        path=p7hsklhahf9l8igbijc4r74vgiaqnm8d/nix-2.34.0-x86_64-linux.tar.xz
         system=x86_64-linux
         ;;
     Linux.i?86)
-        hash=ae4b00e3cbe89b060d0f2548b2a7ae4f361c2856064903ed649736baad102031
-        path=jya581xaqw0pck350lbvbzfmnrqcz1g3/nix-2.29.1-i686-linux.tar.xz
+        hash=34ff599a5e2c737deae1fc8dd33d5799ea5b226360d9224c0c3287ae031d7b69
+        path=lfp03rz1frvxnhvimj2x724lsnw43war/nix-2.34.0-i686-linux.tar.xz
         system=i686-linux
         ;;
     Linux.aarch64)
-        hash=5e7fbf44d4450ce68d2e0db71e6f2bc1cab59cba892c3fb30ada83c71e100ee3
-        path=045g8p2mb4xafgj58w2l17k8340wwdxf/nix-2.29.1-aarch64-linux.tar.xz
+        hash=cfddd4008b57a71464a16d5232cba79b1c76ae9dc81bbf71b4972b0118bc29c5
+        path=9cwmhn5rqs62v6x8bqslrdz07g3f2ccf/nix-2.34.0-aarch64-linux.tar.xz
         system=aarch64-linux
         ;;
     Linux.armv6l)
-        hash=d0d624fe813e05a0df55621cb75d70a7510a2a317c84f9623e241d80d3d0dbef
-        path=a6nsrz9p9ipyijpdl4cjgjbpvlw9dxxz/nix-2.29.1-armv6l-linux.tar.xz
+        hash=7494434b4385d02f84fd9749dd0cf9988f0d6880ebcb24e356c8d4b99ee41d43
+        path=d6ah835n855k4nz4q7lhq735nsis9lfl/nix-2.34.0-armv6l-linux.tar.xz
         system=armv6l-linux
         ;;
     Linux.armv7l)
-        hash=222f755287df338e3fdd24ca059a4adbc955e6c13d7a282a3330927554fd0abb
-        path=7f20rcp0kfal73hkbcp1g2a8ynnh9zy0/nix-2.29.1-armv7l-linux.tar.xz
+        hash=050c7ac67e544bd2f6cdbc6c59381d842a952abceb2fca29f3a5448b47814a5c
+        path=bbbxmm2xasj880xjd0m9zxxp6av6n2q3/nix-2.34.0-armv7l-linux.tar.xz
         system=armv7l-linux
         ;;
     Linux.riscv64)
-        hash=364d5c21a15eb7cb1e3292080d889fb856f0a16e1b898fec0fa39c106992286e
-        path=fskyy31mmwkzwwdfdvhh8hbndkiigc28/nix-2.29.1-riscv64-linux.tar.xz
+        hash=479a2727760930b437f1cbbcc717ee3a9255062254a5e0e3171df2a0e84359e9
+        path=30gmx4nw3fbqk27ks40vjzdy1xam7gyv/nix-2.34.0-riscv64-linux.tar.xz
         system=riscv64-linux
         ;;
     Darwin.x86_64)
-        hash=21912ed2cb17e8354ddc386018e6e75bf4721d35d5962f53206a777822d1308c
-        path=2b8wdc50w6m97fbflfd7ykzhi61s83ga/nix-2.29.1-x86_64-darwin.tar.xz
+        hash=d3d00cba2a309c6c8d12469916f1729221c74f6326f486c51d361f0c797d947f
+        path=gb17z8rhf3qialb8pvcmf11qdspw7w0l/nix-2.34.0-x86_64-darwin.tar.xz
         system=x86_64-darwin
         ;;
     Darwin.arm64|Darwin.aarch64)
-        hash=d91a918fda9e3aebd6892919f43e51e4f959fa60cca188cbefa6039fab0373ad
-        path=2cg7527lqw2arm6j4dmdxzbwq45xm85s/nix-2.29.1-aarch64-darwin.tar.xz
+        hash=47cb78c9fdc7b630dbbb9a89869c8e8bcd8c9eb17be036fba18585120693a4c1
+        path=1jhg8jbdv55r320rd59q0f69jwf2j84r/nix-2.34.0-aarch64-darwin.tar.xz
         system=aarch64-darwin
         ;;
     *) oops "sorry, there is no binary distribution of Nix for your platform";;
@@ -76,10 +76,10 @@ if [ "${1:-}" = "--tarball-url-prefix" ]; then
     url=${2}/${path}
     shift 2
 else
-    url=https://releases.nixos.org/nix/nix-2.29.1/nix-2.29.1-$system.tar.xz
+    url=https://releases.nixos.org/nix/nix-2.34.0/nix-2.34.0-$system.tar.xz
 fi
 
-tarball=$tmpDir/nix-2.29.1-$system.tar.xz
+tarball=$tmpDir/nix-2.34.0-$system.tar.xz
 
 require_util tar "unpack the binary tarball"
 if [ "$(uname -s)" != "Darwin" ]; then
@@ -94,7 +94,7 @@ else
     oops "you don't have wget or curl installed, which I need to download the binary tarball"
 fi
 
-echo "downloading Nix 2.29.1 binary tarball for $system from '$url' to '$tmpDir'..."
+echo "downloading Nix 2.34.0 binary tarball for $system from '$url' to '$tmpDir'..."
 fetch "$url" "$tarball" || oops "failed to download '$url'"
 
 if command -v sha256sum > /dev/null 2>&1; then
